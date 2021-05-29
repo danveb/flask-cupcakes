3283a51103f6c00d40fa9690d3a468e41738fa12
Daniel Bae <danibae91@icloud.com>
2021-05-29 15:24:00 -0500

    part one

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template 
+from models import db, connect_db, Cupcake 
+
+app = Flask(__name__) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SECRET_KEY'] = 'oh-so-super-very-secret'
+
+connect_db(app) 
+# db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
-"""Models for Cupcakes app"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+# Models 
+class Cupcake(db.Model):
+    """Cupcake Model"""
+    __tablename__ = 'cupcakes' 
+    # Flask SQLAlchemy Columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMG_URL) 
+
+# createdb cupcakes / dropdb cupcakes
+# python3 seed.py
